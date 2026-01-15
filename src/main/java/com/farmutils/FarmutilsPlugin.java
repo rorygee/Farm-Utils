fd733f7564faf0fc780b4e42ab3f047a4d802f5e
Rory Gee <rorygee@outlook.com>
2026-01-15 05:13:20 +0000

    Initial Plugin Configuration

--- src/main/java/com/farmutils/FarmutilsPlugin.java
+++ src/main/java/com/farmutils/FarmutilsPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.farmutils;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Farm Utils"
 )
-public class ExamplePlugin extends Plugin
+public class FarmutilsPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private FarmutilsConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	FarmutilsConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(FarmutilsConfig.class);
 	}
 }
