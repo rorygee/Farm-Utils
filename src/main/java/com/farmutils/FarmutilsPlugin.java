10d6068e2adec9084f83cc31d46655b3dfb15553
Rory Gee <rorygee@outlook.com>
2026-01-31 19:04:47 +0000

    Panel Selection Framework

--- src/main/java/com/farmutils/FarmutilsPlugin.java
+++ src/main/java/com/farmutils/FarmutilsPlugin.java
@@ -1,26 +1,22 @@
 package com.farmutils;
 
+import com.farmutils.ui.FarmPanel;
 import com.farmutils.ui.FarmRootPanel;
 import com.farmutils.ui.FarmStubPanel;
 import com.google.inject.Provides;
-import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import com.farmutils.ui.FarmPanel;
-import javax.inject.Inject;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ImageUtil;
-import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
+
+import javax.inject.Inject;
 
 @Slf4j
 @PluginDescriptor(
